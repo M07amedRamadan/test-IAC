b452dbfd268607c56b34a1368b8e1c14b99921fe
M07amedRamadan <mohamed.ramadan.b0@gmail.com>
2024-01-23 00:47:33 +0200

    peering update

--- src/main/vpc-peering.tf
+++ src/main/vpc-peering.tf
@@ -1,36 +1,35 @@
 
-// # Define the second VPC
-// resource "aws_vpc" "vpc2" {
-//   cidr_block = "20.0.0.0/16"
-//   enable_dns_support = true
-//   enable_dns_hostnames = true
-//   tags = {
-//     Name = "VPC2"
-//   }
-// }
+ # Define the second VPC
+ resource "aws_vpc" "vpc2" {
+   cidr_block = "20.0.0.0/16"
+   enable_dns_support = true
+   enable_dns_hostnames = true
+   tags = {
+     Name = "VPC2"
+   }
+ }
 
-// # Create a VPC peering connection
-// resource "aws_vpc_peering_connection" "peering_connection" {
-//   peer_vpc_id = aws_vpc.vpc2.id
-//   vpc_id      = aws_vpc.vpc1.id
+ # Create a VPC peering connection
+ resource "aws_vpc_peering_connection" "peering_connection" {
+   peer_vpc_id = aws_vpc.vpc2.id
+   vpc_id      = aws_vpc.vpc1.id
 
-//   auto_accept = true
+   auto_accept = true
 
-//   tags = {
-//     Name = "VPC-Peering"
-//   }
-// }
+   tags = {
+     Name = "VPC-Peering"
+   }
+ }
 
-// # Create a route in the first VPC's route table to the second VPC via the peering connection
-// resource "aws_route" "route_to_vpc2" {
-//   route_table_id         = aws_vpc.vpc1.main_route_table_id
-//   destination_cidr_block = aws_vpc.vpc2.cidr_block
-//   vpc_peering_connection_id = aws_vpc_peering_connection.peering_connection.id
-// }
-
-// # Create a route in the second VPC's route table to the first VPC via the peering connection
-// resource "aws_route" "route_to_vpc1" {
-//   route_table_id         = aws_vpc.vpc2.main_route_table_id
-//   destination_cidr_block = aws_vpc.vpc1.cidr_block
-//   vpc_peering_connection_id = aws_vpc_peering_connection.peering_connection.id
-// }
+ # Create a route in the first VPC's route table to the second VPC via the peering connection
+ resource "aws_route" "route_to_vpc2" {
+   route_table_id         = aws_vpc.vpc1.main_route_table_id
+   destination_cidr_block = aws_vpc.vpc2.cidr_block
+   vpc_peering_connection_id = aws_vpc_peering_connection.peering_connection.id
+ }
+ # Create a route in the second VPC's route table to the first VPC via the peering connection
+ resource "aws_route" "route_to_vpc1" {
+   route_table_id         = aws_vpc.vpc2.main_route_table_id
+   destination_cidr_block = aws_vpc.vpc1.cidr_block
+   vpc_peering_connection_id = aws_vpc_peering_connection.peering_connection.id
+ }
