1d75f7020d64e703ec487d4c7f8428cc657c9195
M07amedRamadan <mohamed.ramadan.b0@gmail.com>
2024-01-22 19:06:13 +0200

    Update main.tf

--- src/main.tf
+++ src/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 # Define the first VPC
 resource "aws_vpc" "Main-VPC"" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = var.cidr_block
   enable_dns_support = true
   enable_dns_hostnames = true
   tags = {
