1ca9e688bf46659043f25d2f5b69bd31b26a6ff9
M07amedRamadan <mohamed.ramadan.b0@gmail.com>
2024-01-22 19:42:38 +0200

    Update variables.tf

--- src/variables.tf
+++ src/variables.tf
@@ -1,19 +1,19 @@
 variable "region"{
 type = string
-default= var("TF_region")
+default= var.TF_region
 }
 
 variable "cidr_block"{
 type = string
-default= var("TF_cidr_block")
+default= var.TF_cidr_block
 }
 
 variable "secret_key"{
 type = string
-default= var("TF_secret_key")
+default= var.TF_secret_key
 }
 
 variable "access_key"{
 type = string
-default= var("TF_access_key")
+default= var.TF_access_key
 }
