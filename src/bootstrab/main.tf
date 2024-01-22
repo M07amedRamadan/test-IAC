c1836ab6a4fbbc1a7a406c9f84378a7f0dc5b97a
M07amedRamadan <mohamed.ramadan.b0@gmail.com>
2024-01-22 22:39:06 +0200

    Update main.tf

--- src/bootstrab/main.tf
+++ src/bootstrab/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "statebucket" {
-  bucket = "terraform.tfstate-Bucket"
+  bucket = "terraform.tfstate-bucket"
 
   tags = {
    # Name        = "My bucket"
