e6b578aece52df70a8b5e41989544df49ee789d6
M07amedRamadan <mohamed.ramadan.b0@gmail.com>
2024-01-23 19:32:26 +0200

    update

--- src/main/ec2-instance.tf
+++ src/main/ec2-instance.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "Instance1" {
   ami           = var.ami
   instance_type = var.instance_type
   #key_name               = "vultara-report-server-KP"
-  vpc_security_group_ids = [aws_security_group.example_security_group.name]
+  vpc_security_group_ids = [aws_security_group.example_security_group.id]
   subnet_id              = aws_subnet.public_subnet.id
   iam_instance_profile   = aws_iam_role.example_role.name
   tags = {
@@ -15,7 +15,7 @@ resource "aws_instance" "Instance2" {
   ami           = var.ami
   instance_type = var.instance_type
   #key_name               = "vultara-trial-scheduler-KP"
-  vpc_security_group_ids = [aws_security_group.schedulerServer_SG.id]
+  vpc_security_group_ids = [aws_security_group.example_security_group.id]
   subnet_id              = aws_subnet.public_subnet.id
   iam_instance_profile   = aws_iam_role.example_role.name
   tags = {
