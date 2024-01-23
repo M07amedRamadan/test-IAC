dcbd7b5ce0d752a1dedd073b5645a98ff5c73094
M07amedRamadan <mohamed.ramadan.b0@gmail.com>
2024-01-23 21:29:57 +0200

    update

--- src/main/ec2-instance.tf
+++ src/main/ec2-instance.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "Instance1" {
   vpc_security_group_ids = [aws_security_group.example_security_group.id]
   subnet_id              = aws_subnet.public_subnet.id
   iam_instance_profile   =  aws_iam_instance_profile.instance_profile.name
-  key_name               = "key_name"
+  key_name               = "bakry"
   tags = {
     Name = "${var.CUSTOMER_NAME}-ec1"
   }
@@ -19,7 +19,7 @@ resource "aws_instance" "Instance2" {
   vpc_security_group_ids = [aws_security_group.example_security_group.id]
   subnet_id              = aws_subnet.public_subnet.id
   iam_instance_profile   =  aws_iam_instance_profile.instance_profile.name
-  key_name               = "key_name"
+  key_name               = "bakry"
 
 
   tags = {
