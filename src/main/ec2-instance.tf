96328e89a80efaa18fee4a6b799fd2ad55c87744
M07amedRamadan <mohamed.ramadan.b0@gmail.com>
2024-01-23 19:54:19 +0200

    update

--- src/main/ec2-instance.tf
+++ src/main/ec2-instance.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "Instance1" {
   #key_name               = "vultara-report-server-KP"
   vpc_security_group_ids = [aws_security_group.example_security_group.id]
   subnet_id              = aws_subnet.public_subnet.id
-  iam_instance_profile   = aws_iam_role.example_role.name
+  iam_instance_profile   =  aws_iam_instance_profile.instance_profile.name
   tags = {
     Name = "${var.CUSTOMER_NAME}-ec1"
   }
@@ -17,9 +17,16 @@ resource "aws_instance" "Instance2" {
   #key_name               = "vultara-trial-scheduler-KP"
   vpc_security_group_ids = [aws_security_group.example_security_group.id]
   subnet_id              = aws_subnet.public_subnet.id
-  iam_instance_profile   = aws_iam_role.example_role.name
+  iam_instance_profile   =  aws_iam_instance_profile.instance_profile.name
   tags = {
     Name = "${var.CUSTOMER_NAME}-ec2"
   }
  
+}
+
+# IAM Instance Profile
+# Used to attach iam-role with ec2 
+resource "aws_iam_instance_profile" "instance_profile" {
+  name = "Instance-profile"
+  role = aws_iam_role.example_role.name 
 }
