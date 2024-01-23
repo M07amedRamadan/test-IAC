b42cc33aa3c3d810be13206c8cce4f7653db8471
M07amedRamadan <mohamed.ramadan.b0@gmail.com>
2024-01-23 21:07:53 +0200

    update

--- src/main/ec2-instance.tf
+++ src/main/ec2-instance.tf
@@ -18,16 +18,9 @@ resource "aws_instance" "Instance2" {
   vpc_security_group_ids = [aws_security_group.example_security_group.id]
   subnet_id              = aws_subnet.public_subnet.id
   iam_instance_profile   =  aws_iam_instance_profile.instance_profile.name
-
   tags = {
     Name = "${var.CUSTOMER_NAME}-ec2"
   }
  
 }
 
-# IAM Instance Profile
-# Used to attach iam-role with ec2 
-resource "aws_iam_instance_profile" "instance_profile" {
-  name = "Instance-profile"
-  role = aws_iam_role.example_role.name 
-}
--- src/main/iam-role.tf
+++ src/main/iam-role.tf
@@ -3,8 +3,9 @@ data "aws_iam_role" "existing_role" {
   name = "bakryCloudWatch"  # Replace with the name of your existing IAM role
 }
 
+# IAM Instance Profile
+# Used to attach iam-role with ec2 
 resource "aws_iam_instance_profile" "instance_profile" {
-  name = "instance-profile"
-
+  name = "Instance-profile"
   role = data.aws_iam_role.existing_role.name
 }
