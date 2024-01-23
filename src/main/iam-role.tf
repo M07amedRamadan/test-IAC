aed30b71c8376528b3ac98c0eea7d915438dc38e
M07amedRamadan <mohamed.ramadan.b0@gmail.com>
2024-01-23 20:51:59 +0200

    update

--- src/main/iam-role.tf
+++ src/main/iam-role.tf
@@ -1,10 +1,10 @@
 
 data "aws_iam_role" "existing_role" {
-  name = "your_existing_role_name"  # Replace with the name of your existing IAM role
+  name = "bakryCloudWatch"  # Replace with the name of your existing IAM role
 }
 
-resource "aws_iam_instance_profile" "example_instance_profile" {
-  name = "example-instance-profile"
+resource "aws_iam_instance_profile" "instance_profile" {
+  name = "instance-profile"
 
   role = data.aws_iam_role.existing_role.name
 }
