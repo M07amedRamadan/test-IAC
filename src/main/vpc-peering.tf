704be58408747d22807f2bdc5ae13e0e494fa0f3
M07amedRamadan <mohamed.ramadan.b0@gmail.com>
2024-01-24 01:08:24 +0200

    update

--- src/main/vpc-peering.tf
+++ src/main/vpc-peering.tf
@@ -1,3 +1,4 @@
+#peer region to be used at accepter resource.
 provider "aws" {
   alias  = "peer"
   region = "us-west-1"
@@ -10,11 +11,10 @@ provider "aws" {
    peer_region = "us-west-1"
    peer_vpc_id = "vpc-0528c65dcd5e11cee"
    vpc_id      = aws_vpc.Main-VPC.id
-
-   #auto_accept = true
    
    tags = {
      Name = "VPC-Peering"
+     Side = "Requester"
    }
  }
 
@@ -44,20 +44,19 @@ provider "aws" {
  
 
 
-# data "aws_vpcs" "example" {
-#   filter {
-#     id = "vpc-0528c65dcd5e11cee"  # Replace with the ID of your VPC
-#   }
-  
-# }
+data "aws_vpcs" "example" {}
+
+data "aws_route_table" "default_route_table" {
+  vpc_id = data.aws_vpcs.example.ids[0]
+}
 
 # data "aws_route_table" "default_route_table" {
 #   vpc_id = data.aws_vpcs.example.ids[0]
 # }
 
-# output "default_route_table_id" {
-#   value = data.aws_route_table.default_route_table.id
-# }
+output "default_route_table_id" {
+  value = data.aws_route_table.default_route_table.id
+}
 # output "vpc_cidr_block" {
 #   value = data.aws_vpcs.example.cidr_blocks[0]
 # }
