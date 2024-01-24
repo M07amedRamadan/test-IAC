c1f3a04cfabb6e6d46ca45f452885537393e5157
M07amedRamadan <mohamed.ramadan.b0@gmail.com>
2024-01-25 00:54:36 +0200

    update

--- src/main/s3-endpoint.tf
+++ src/main/s3-endpoint.tf
@@ -1,7 +1,6 @@
 resource "aws_vpc_endpoint" "s3_endpoint" {
     vpc_id      = aws_vpc.Main-VPC.id
     service_name = "com.amazonaws.us-west-2.s3"  # Adjust the region
-    security_group_ids = [aws_security_group.example_security_group.id]
 
     tags = {Name = "s3_endpoint"}
 
