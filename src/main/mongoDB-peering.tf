a773dd284bfbad74a76c26e3a2ee2408524debbf
M07amedRamadan <mohamed.ramadan.b0@gmail.com>
2024-01-24 18:37:47 +0200

    update

--- src/main/mongoDB-peering.tf
+++ src/main/mongoDB-peering.tf
@@ -26,23 +26,3 @@ resource "aws_route" "my_route" {
   vpc_peering_connection_id = aws_vpc_peering_connection.my_peering_connection.id
 }
 
-# Add MongoDB Atlas CIDR block to security group rules
-resource "aws_security_group" "mongodb_security_group" {
-  name        = "mongodb_security_group"
-  description = "Security group for MongoDB Atlas"
-  vpc_id      = aws_vpc.Main-VPC.id
-#this is the input rule which is accept connection from port 27017 to port 27017 from the mongodb cidr_block
-  ingress {
-    from_port = 27017
-    to_port   = 27017
-    protocol  = "tcp"
-    cidr_blocks = [var.mongodb_atlas_cidr]
-  }
-#this is the output rule which is accept all connections from and to any port to the mongodb cidr_block
-  egress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
-    cidr_blocks = [var.mongodb_atlas_cidr]
-  }
-}
