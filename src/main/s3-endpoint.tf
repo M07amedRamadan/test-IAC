352a5d09e93805df1b0cdb1bb8377eeb2a948097
M07amedRamadan <mohamed.ramadan.b0@gmail.com>
2024-01-25 00:34:06 +0200

    update

--- src/main/s3-endpoint.tf
+++ src/main/s3-endpoint.tf
@@ -1,16 +1,19 @@
 resource "aws_vpc_endpoint" "s3_endpoint" {
-  vpc_id      = aws_vpc.Main-VPC.id
-  service_name = "com.amazonaws.us-west-2.s3"  # Adjust the region
+    vpc_id      = aws_vpc.Main-VPC.id
+    service_name = "com.amazonaws.us-west-2.s3"  # Adjust the region
+    security_group_ids = [aws_security_group.example_security_group.id]
+    subnet_ids          = [aws_subnet.private_subnet.id]
+
+    tags = {Name = "s3_endpoint"}
+
 }
 
-resource "aws_route_table_association" "s3_endpoint_association" {
-  subnet_id      = aws_subnet.private_subnet.id  # Replace with your private subnet ID
-  route_table_id = aws_vpc.Main-VPC.main_route_table_id # Replace with your route table ID
-}
+# resource "aws_route_table_association" "s3_endpoint_association" {
+#   subnet_id      = aws_subnet.private_subnet.id  # Replace with your private subnet ID
+#   route_table_id = aws_vpc.Main-VPC.main_route_table_id # Replace with your route table ID
+# }
 
-output "s3_bucket_endpoint" {
-  value = aws_vpc.Main-VPC.main_route_table_id
-}
+
 
 # output "s3_bucket_endpoint" {
 #   value = aws_vpc_endpoint.s3_endpoint.
