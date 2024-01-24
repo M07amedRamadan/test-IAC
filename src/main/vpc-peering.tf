7a197cca88a4c9690e82b230e5880ccb425574d2
M07amedRamadan <mohamed.ramadan.b0@gmail.com>
2024-01-24 02:02:57 +0200

    update

--- src/main/vpc-peering.tf
+++ src/main/vpc-peering.tf
@@ -48,3 +48,6 @@ data "aws_vpc" "Existance_VPC" {
   id = "vpc-0fd5c529a45ac6978"  # Replace with the ID of your VPC
 }
 
+output "Existance_VPC_main_route_table_id" {
+  value = data.aws_vpc.Existance_VPC.main_route_table_id
+}
