2fab9874951ebd2586b53e03c8db1f4d2a5fbb17
M07amedRamadan <mohamed.ramadan.b0@gmail.com>
2024-01-25 03:22:31 +0200

    update

---
+++ src/main/ami-copy.tf
@@ -0,0 +1,11 @@
+# provider "aws" {
+#   region = "us-west-2"  # Specify the source region
+# }
+
+# data "aws_ami_copy" "copied_ami" {
+#   source_ami_id = "ami-xxxxxxxxxxxxxxxxx"  # Specify the source AMI ID
+#   source_region = "us-west-2"               # Specify the source region
+
+#   name        = "Copied AMI Name"            # Specify the name for the copied AMI
+#   description = "Copied AMI Description"     # Specify the description for the copied AMI
+# }
--- src/main/secret_manager.tf
+++ src/main/secret_manager.tf
@@ -1,14 +1,14 @@
-#No need to change only ad your keys/values
-resource "aws_secretsmanager_secret" "example_secret" {
-  name = "second-secret"
-}
+# #No need to change only ad your keys/values
+# resource "aws_secretsmanager_secret" "example_secret" {
+#   name = "second-secret"
+# }
 
-resource "aws_secretsmanager_secret_version" "example_secret_version" {
-  secret_id = aws_secretsmanager_secret.example_secret.id
-  secret_string = jsonencode({
-    key1 = "value1",
-    key2 = "value2",
-    key3 = "value3",
-    // Add more key-value pairs as needed
-  })
-}
+# resource "aws_secretsmanager_secret_version" "example_secret_version" {
+#   secret_id = aws_secretsmanager_secret.example_secret.id
+#   secret_string = jsonencode({
+#     key1 = "value1",
+#     key2 = "value2",
+#     key3 = "value3",
+#     // Add more key-value pairs as needed
+#   })
+# }
