1fad0d1faf9e123bb52fef21cab67244819781c9
M07amedRamadan <mohamed.ramadan.b0@gmail.com>
2024-02-27 00:25:07 +0200

    Update main.tf

--- testDir/main.tf
+++ testDir/main.tf
@@ -4,14 +4,17 @@ provider "aws" {
 
 resource "aws_s3_bucket" "s3_bucket" {
   bucket = "test.vultara.com"
-  acl    = "private"
+}
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.s3_bucket.id
 
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
-  
 }
+
 
 # Update the bucket policy
 resource "aws_s3_bucket_policy" "s3_bucket_policy" {
