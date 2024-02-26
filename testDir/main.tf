d5ee7a4f510ac9fb1b2e59907ca7bcf442a0df4a
M07amedRamadan <mohamed.ramadan.b0@gmail.com>
2024-02-26 23:33:44 +0200

    Update main.tf

--- testDir/main.tf
+++ testDir/main.tf
@@ -2,16 +2,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-#check if there is an bucket with same name it will not create anothe one just use existance.
-resource "aws_s3_bucket" "statebucket" {
-count = data.aws_s3_bucket.existing_bucket != null ? 0 : 1 # If the bucket exists, don't create it
-  bucket = "terraform.tfstate-bucket-ramadan"
-}
-
-output "bucket_message" {
-  value = data.aws_s3_bucket.existing_bucket != null ? "Bucket already exists!" : "Bucket created successfully!"
-}
-
 resource "aws_s3_bucket" "s3_bucket" {
   bucket = "${var.CUSTOMER_NAME}.vultara.com"
   acl    = "private"
