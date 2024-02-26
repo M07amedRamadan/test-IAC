03e6a5316506cce7144622240cd8b6ed841ca34e
M07amedRamadan <mohamed.ramadan.b0@gmail.com>
2024-02-26 22:11:56 +0200

    Create main.tf

---
+++ testDir/main.tf
@@ -0,0 +1,34 @@
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = "${var.CUSTOMER_NAME}.vultara.com"
+  acl    = "private"
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+  
+}
+
+# Update the bucket policy
+resource "aws_s3_bucket_policy" "s3_bucket_policy" {
+  bucket = aws_s3_bucket.s3_bucket.id
+  
+  policy = <<EOF
+    {
+      "Version": "2008-10-17",
+      "Id": "PolicyForCloudFrontPrivateContent",
+      "Statement": [
+        {
+          "Sid": "1",
+          "Effect": "Allow",
+          "Principal": {
+            "AWS": "arn:aws:iam::cloudfront:user/CloudFront Origin Access Identity ramadan}"
+          },
+          "Action": "s3:GetObject",
+          "Resource": "arn:aws:s3:::${var.CUSTOMER_NAME}.vultara.com/*"
+        }
+      ]
+    }
+  EOF
+
+}
